6b48f5444c3d1c55b4a44319bbc1cd6b6aeb17a8
vektor8891 <vszabo@protonmail.com>
2021-11-20 00:07:34 -0500

    reset data

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import lib.globals as g
 import lib.youtube_thumbnails as t
 import lib.youtube_comments as c
 
-client_id = 2
+client_id = 1
 env = 'prod'  # test/prod
 status = 'public'  # public/unlisted/private
 create_new_connection = 0
@@ -17,13 +17,13 @@ conn = a.get_connection(create_new=create_new_connection, client_id=client_id)
 channel.print_info(youtube=conn)
 v.compare_videos(env, conn)
 
-# id_start = 28
-# id_end = 162
-# video_ids = list(range(id_start, id_end + 1))
-video_ids = [1]
+id_start = 1
+id_end = 157
+video_ids = list(range(id_start, id_end + 1))
+# video_ids = [1]
 
-for video_id in video_ids:
-    print(v.get_markdown(video_id=video_id, env=env))
+# for video_id in video_ids:
+#     print(v.get_markdown(video_id=video_id, env=env))
 
 # v.add_videos(youtube=conn, video_ids=video_ids, status=status, env=env)
 # v.update_videos(youtube=conn, video_ids=video_ids, status=status, env=env)
